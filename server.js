67bfb2db6b7e20fd50af6ae9b0af6fcddddca77d
viola444 <viola.iranpour@iths.se>
2025-01-20 11:50:27 +0100

    connectionstring

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require("express");
-const app = express();
+const app = express()
+const mongoose = require("mongoose");
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.send("welcome to my mongoAPI");
+});
+
+app.post("/blogpost", (req, res) => {
+    res.send("this is a postrequest");
+});
+
+
+mongoose.connect("mongodb+srv://violair:123hej@violasapi.x2zne.mongodb.net/Products-api?retryWrites=true&w=majority&appName=ViolasAPI")
+.then(() => {
+    console.log("connected to mongo");
+    
 app.listen(PORT, () => {
     console.log("listeing to 3000");
+    });
 });
+
