658a296dd7dfc993f65830c78a851d35595f10fc
viola444 <viola.iranpour@iths.se>
2025-01-20 15:08:23 +0100

    lektion slutproduct

---
+++ models/productModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const productSchema = mongoose.Schema(
+    {
+        name:{
+            type: String,
+            required: true
+        }, 
+        quantity:  {
+            type: Number,
+            required: true,
+            default: 0
+        }, 
+        price: {
+            type:Number,
+            required: true
+        },
+        image: {
+            type: String,
+            required: false
+        },
+            
+    }
+);
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
--- server.js
+++ server.js
@@ -1,18 +1,68 @@
+require("dotenv").config();
 const express = require("express");
 const app = express()
 const mongoose = require("mongoose");
 const PORT = 3000;
+const Product = require("./models/productModel");
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("welcome to my mongoAPI");
 });
 
-app.post("/blogpost", (req, res) => {
+app.post("/blogpost",(req, res) => {
     res.send("this is a postrequest");
 });
 
+app.put ("/api/product/:id", async (req, res) => {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
 
-mongoose.connect("mongodb+srv://violair:123hej@violasapi.x2zne.mongodb.net/Products-api?retryWrites=true&w=majority&appName=ViolasAPI")
+    if (!product) {
+        return res.status(400).json({message: " Product does not exist"});
+    }
+    res.status(200).json(product);
+});
+
+app.get("/api/product/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500);
+    }
+});
+
+
+
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+    } catch (error) {
+      res.status(500).json({message: error.mesage});
+    }
+});
+
+app.post("/api/product", async (req, res) => {
+   
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(404);
+    }
+});
+
+/*
+app.post("/api/product",(req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+}); */
+
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("connected to mongo");
     
